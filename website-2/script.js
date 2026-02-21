4cd66b1402949e77d30459f6742dbfe4942e4b7d
smdarshad000-lab <smdarshad000@gmail.com>
2026-02-21 09:24:54 +0530

    done with 2nd website

---
+++ website-2/script.js
@@ -0,0 +1,53 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function videoconAnimation(){
+    var videocon = document.querySelector("#video-container");
+    var playbtn = document.querySelector("#play");
+
+    videocon.addEventListener("mouseenter", function(){
+        gsap.to(playbtn,{
+            scale:1,
+            opacity:1,
+        });
+    });
+
+    videocon.addEventListener("mouseleave", function(){
+        gsap.to(playbtn,{
+            scale:0,
+            opacity:0,
+        });
+    });
+
+    videocon.addEventListener("mousemove", function(dets){
+        gsap.to(playbtn,{
+            left: dets.clientX - 70,
+            top: dets.clientY - 80,
+        });
+    });
+}
+
+ videoconAnimation()
+
+ function loadinganimetion(){
+    gsap.from("#page1 h1",{
+    y: 30,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.2,
+ })
+     gsap.from("#page1 #video-container",{
+    scale: 0.8,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.3,
+ })
+}
+
+ loadinganimetion()
+ 
+
+
